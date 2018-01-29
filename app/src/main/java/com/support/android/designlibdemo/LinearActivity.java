44fd07fc65c6d460487563f4ce955bc999aa1169
PandaPan <280853071@qq.com>
2018-01-29 22:36:55 +0800

    删除没必要的floatActionButton

--- app/src/main/java/com/support/android/designlibdemo/LinearActivity.java
+++ app/src/main/java/com/support/android/designlibdemo/LinearActivity.java
@@ -53,15 +53,6 @@ public class LinearActivity extends AppCompatActivity {
             setupViewPager(viewPager);
         }
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Here's a Snackbar", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(viewPager);
     }
